2ad9280365ea47f065903c188609cf4688463be7
Christian Ayscue <coayscue@gmail.com>
2016-02-22 22:27:13 -0800

    arr

--- testCreateNewEvent.js
+++ testCreateNewEvent.js
@@ -1,7 +1,7 @@
 var SC = require('soundclouder');
 var client_id = "bd30924b4a322ba9e488c06edc73f909";
 var client_secret = "f09ab9b33abcefcb2dacdc58fb2b5558";
-var redirect_uri = "http://songsubmission.herokuapp.com/callback.html";
+var redirect_uri = "http://tracksubmission.herokuapp.com/callback.html";
 SC.init(client_id, client_secret, redirect_uri);
 
 SC.get('/users/198031384', function(err, data) {
