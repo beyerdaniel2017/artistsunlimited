bf2b7155359a8b3602fb7e48effb2fc40ed1a2da
Christian Ayscue <coayscue@gmail.com>
2016-02-24 18:37:22 -0800

    folder names

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,7 +6,7 @@ router.use('/channels', require('./channels/channels.js'));
 router.use('/submissions', require('./submissions/submissions.js'));
 router.use('/login', require('./login/login.js'));
 router.use('/events', require('./events/events.js'));
-router.use('/soundcloud', require('./soundCloud/soundCloud.js'));
+router.use('/soundcloud', require('./soundcloud/soundcloud.js'));
 
 // Make sure this is after all of
 // the registered routes!
