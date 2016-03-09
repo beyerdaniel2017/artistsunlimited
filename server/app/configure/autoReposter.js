978ea22aa5517827ece2b0a2e5f70a95c3359465
Christian Ayscue <coayscue@gmail.com>
2016-03-08 17:41:47 -0800

    fix

--- server/app/configure/autoReposter.js
+++ server/app/configure/autoReposter.js
@@ -13,7 +13,7 @@ module.exports = function() {
 function doRepost() {
   setTimeout(function() {
     doRepost();
-  }, 1200000);
+  }, 10000);
 
   var date = new Date();
   var hour = date.getHours();
@@ -56,17 +56,19 @@ function repostAndRemove(event, channel) {
     uri: scConfig.redirectURL,
     accessToken: channel.accessToken
   });
-  SC.delete('/e1/me/track_reposts/' + id, function(err, data) {
-    SC.put('/e1/me/track_reposts/' + id, function(err, data) {
-      if (err) {
-        sendMessage("CHRISTIAN", "coayscue@gmail.com", "ME", "coayscue@gmail.com", "ERROR REPOSTING", err.message + "   Event:" + JSON.stringify(event));
-      } else {
-        if (event.email) {
-          sendMessage(event.name, event.email, "Edward Sanchez", "edward@peninsulamgmt.com", "Music Submission", "Hey " + event.name + ",<br><br>We would just like to let you know the track has been reposted on <a href='" + channel.url + "'>" + channel.displayName + "</a>! If you would like to do another round of reposts please resubmit your track to bit.ly/SoundCloudSubmission. We will get back to you ASAP and continue to do our best in making our submission process as quick and easy as possible.<br><br>How was this experience by the way? Feel free to email some feedback, suggestions or just positive reviews to feedback@peninsulamgmt.com.<br><br>Goodluck and thanks again!<br><br>Kevin Zimmermann and Edward Sanchez<br> Peninsula MGMT Team <br>www.facebook.com / kevinlatropical<br> www.facebook.com / edwardlatropical");
+  if (id) {
+    SC.delete('/e1/me/track_reposts/' + id, function(err, data) {
+      SC.put('/e1/me/track_reposts/' + id, function(err, data) {
+        if (err) {
+          sendMessage("CHRISTIAN", "coayscue@gmail.com", "ERROR", "coayscue@gmail.com", "ERROR REPOSTING", "Error: " + err + "\n   Event:" + JSON.stringify(event));
+        } else {
+          if (event.email) {
+            sendMessage(event.name, event.email, "Edward Sanchez", "edward@peninsulamgmt.com", "Music Submission", "Hey " + event.name + ",<br><br>We would just like to let you know the track has been reposted on <a href='" + channel.url + "'>" + channel.displayName + "</a>! If you would like to do another round of reposts please resubmit your track to bit.ly/SoundCloudSubmission. We will get back to you ASAP and continue to do our best in making our submission process as quick and easy as possible.<br><br>How was this experience by the way? Feel free to email some feedback, suggestions or just positive reviews to feedback@peninsulamgmt.com.<br><br>Goodluck and thanks again!<br><br>Kevin Zimmermann and Edward Sanchez<br> Peninsula MGMT Team <br>www.facebook.com / kevinlatropical<br> www.facebook.com / edwardlatropical");
+          }
+          Event.findByIdAndRemove(event._id).exec();
         }
-        Event.findByIdAndRemove(event._id).exec();
-      }
-    });
-  })
+      });
+    })
+  }
 
 }
