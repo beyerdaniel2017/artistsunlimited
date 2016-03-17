95568c96c3c2e079a0053b9b64b2f3502e4ecf9e
Christian Ayscue <coayscue@gmail.com>
2016-03-16 22:30:51 -0700

    switched to aws

--- testSoundcloud.js
+++ testSoundcloud.js
@@ -8,31 +8,6 @@ SC.init({
   secret: client_secret,
   uri: redirect_uri
 });
-var accessToken = "1-183955-164339022-d8322239ae0ce";
-var id = 86560544;
-SC.get('/users/' + id + '/web-profiles', function(err, webProfiles) {
-  console.log(err);
-  var follower = {};
-  if (webProfiles) {
-    for (var index in webProfiles) {
-      console.log(index);
-      switch (webProfiles[index].service) {
-        case 'twitter':
-          follower.twitterURL = webProfiles[index].url;
-          break;
-        case 'instagram':
-          follower.instagramURL = webProfiles[index].url;
-          break;
-        case 'facebook':
-          follower.facebookURL = webProfiles[index].url;
-          break;
-        case 'youtube':
-          follower.youtubeURL = webProfiles[index].url;
-          break;
-        case 'personal':
-          follower.websites += webProfiles[index].url + '\n';
-      }
-    }
-  }
-  console.log(follower);
+SC.get('/tracks/' + process.argv[2], function(err, response) {
+  console.log(response.permalink_url);
 });
