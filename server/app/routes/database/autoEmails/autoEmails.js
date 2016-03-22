a1b88098c45e05dc968e001419dcd7d61dd6f391
Virendra Chouhan <chouhan.virendra@gmail.com>
2016-03-22 19:20:59 +0530

    #15 updated email template functionality and view

--- server/app/routes/database/autoEmails/autoEmails.js
+++ server/app/routes/database/autoEmails/autoEmails.js
@@ -37,7 +37,7 @@ router.post('/', function(req, res, next) {
     EmailTemplate
       .findOneAndUpdate({
         _id: update._id
-      }, update)
+      }, { $set: update})
       .exec()
       .then(function(template) {
         return res.send(template);
