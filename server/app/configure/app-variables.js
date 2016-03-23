863be29d9fda4fb528cf24fdefc744d0002ef569
Christian Ayscue <coayscue@gmail.com>
2016-03-23 11:19:16 -0700

    logging done

--- server/app/configure/app-variables.js
+++ server/app/configure/app-variables.js
@@ -11,7 +11,8 @@ var faviconPath = path.join(rootPath, './server/app/views/favicon.ico');
 var env = require(path.join(rootPath, './server/env'));
 
 var logMiddleware = function(req, res, next) {
-    util.log('------------------------------------------------');
+    console.log('------------------------------------------------')
+    util.log('');
     console.log(util.format(chalk.red('%s: %s %s'), 'REQUEST ', req.method, req.path));
     console.log(util.format(chalk.yellow('%s: %s'), 'QUERY   ', util.inspect(req.query)));
     console.log(util.format(chalk.cyan('%s: %s'), 'BODY    ', util.inspect(req.body)));
