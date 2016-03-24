687a78dc694f846a8a232fea875ff16905df4a77
Christian Ayscue <coayscue@gmail.com>
2016-03-24 13:48:16 -0700

    ssl

--- testSoundcloud.js
+++ testSoundcloud.js
@@ -2,15 +2,15 @@ var SC = require('node-soundcloud');
 SCResolve = require('soundcloud-resolve-jsonp/node');
 var Promise = require('promise');
 
-var client_id = "3947f8d665f6c6c58e865f894798eb3e";
-var client_secret = "8586b9dbb45ce4c062d23efac6ad2f27";
-var redirect_uri = "http://localhost:1337/callback.html";
+// var client_id = "3947f8d665f6c6c58e865f894798eb3e";
+// var client_secret = "8586b9dbb45ce4c062d23efac6ad2f27";
+// var redirect_uri = "http://localhost:1337/callback.html";
 
-SC.init({
-  id: process.env.SOUNDCLOUD_CLIENT_ID,
-  secret: process.env.SOUNDCLOUD_CLIENT_SECRET,
-  uri: process.env.SOUNDCLOUD_CALLBACK_URL
-});
+// SC.init({
+//   id: process.env.SOUNDCLOUD_CLIENT_ID,
+//   secret: process.env.SOUNDCLOUD_CLIENT_SECRET,
+//   uri: process.env.SOUNDCLOUD_CALLBACK_URL
+// });
 
 (new Promise(function(fulfill, reject) {
   SCResolve({
