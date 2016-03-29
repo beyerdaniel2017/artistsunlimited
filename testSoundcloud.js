e533b8e6d2ef69188b24c0e78903aa47638f76a1
Christian Ayscue <coayscue@gmail.com>
2016-03-28 21:32:10 -0700

    email changes

--- testSoundcloud.js
+++ testSoundcloud.js
@@ -2,6 +2,7 @@ var SC = require('node-soundcloud');
 var SCR = require('soundclouder');
 var SCResolve = require('soundcloud-resolve-jsonp/node');
 var Promise = require('bluebird');
+var HTTPS = require('https');
 
 var client_id = "1e57616d882f00c402ff95abd1f67b39";
 var client_secret = "0934d66f53f5dcd06a1ce9f32e9a6267";
@@ -32,10 +33,32 @@ var redirect_uri = "http://localhost:1337/callback.html";
 //     console.log(err);
 //   })
 
-var aT = "1-231090-203984864-4391af1070f8c";
-var id = 251831353;
+// HTTPS.request({
+//     hostname: 'api.soundcloud.com',
+//     path: '/e1/me/track_reposts/'+id,
+//     method: 'PUT'
+//   }, function(res) {
+//     var dataChunk = '';
+//     res.on('data', function(data) {
+//       dataChunk += data;
+//     });
+//     res.on('end', function() {
+//       console.log(dataChunk);
+//     })
+//   })
+//   .on('error', function(err) {
+//     throw err;
+//   })
+//   .end();
+
+var aT = "1-183955-147045855-0b443e23cd3ba";
+var id = 253638850;
 SCR.init(client_id, client_secret, redirect_uri);
+var log = require("dysf.utils").logger
+log.setLogLevel(5);
 SCR.put('/e1/me/track_reposts/' + id, aT, function(err, data) {
-  console.log();
+  if (err) {
+    throw err;
+  }
   console.log(data);
 });
