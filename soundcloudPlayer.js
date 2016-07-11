5f9fb484fc391d0a1e37dbac8cf1cd61b5a64b4b
Christian Ayscue <coayscue@gmail.com>
2016-07-10 23:57:54 -0600

    bots

--- soundcloudPlayer.js
+++ soundcloudPlayer.js
@@ -3,7 +3,7 @@ var http = require('http');
 var Promise = require('promise');
 var trackID = 272922789;
 var totalSuccesses = 0;
-var accessTokenArray = ['ff023a620394846dc2abb3cf6b48e4ea', '134e3bd683d8b4772be6b8c29074b029', '8c0fa73ad5c07f83cb21b257b958daad'];
+var accessTokenArray = ['ff023a620394846dc2abb3cf6b48e4ea', '134e3bd683d8b4772be6b8c29074b029', '8c0fa73ad5c07f83cb21b257b958daad', '38faf7d0e959c949499860f6443afa66', '281e06c3436513c40ff4bc57b57c3d61'];
 var ipArray = fs.readFileSync('./proxyList.txt').toString().replace(/(\r\n|\n|\r)/g, '\n').split('\n');
 var ipIndex = 0;
 for (var i = 0; i < 100; i++) {
@@ -12,14 +12,14 @@ for (var i = 0; i < 100; i++) {
 
 function runPlayer() {
   if (ipArray[ipIndex]) {
-    ipIndex++;
+    ipIndex = (ipIndex + 1) % (ipArray.length - 2);
     var ipAndPort = ipArray[ipIndex].split(':');
     requestPromise(ipAndPort)
       .then(function(res) {
         console.log(res);
         runPlayer();
       }, function(err) {
-        console.log(err);
+        // console.log(err);
         runPlayer();
       })
   }
@@ -30,12 +30,11 @@ var body = '';
 
 function requestPromise(ipAndPort) {
   return new Promise(function(resolve, reject) {
-    // console.log('http://api.soundcloud.com/tracks/' + trackID + '/stream?client_id=' + accessTokenArray[Math.floor(Math.random() * 3)]);
     var req = http.request({
       host: ipAndPort[0],
       port: ipAndPort[1],
       method: 'GET',
-      path: 'http://api.soundcloud.com/tracks/' + trackID + '/stream?client_id=' + accessTokenArray[Math.floor(Math.random() * 3)],
+      path: 'https://api.soundcloud.com/tracks/' + trackID + '/stream?client_id=' + accessTokenArray[Math.floor(Math.random() * 5)],
       headers: {
         connection: 'keep-alive'
       },
@@ -45,7 +44,7 @@ function requestPromise(ipAndPort) {
         body += data;
       });
       res.on('end', function() {
-        console.log(body);
+        // console.log(body);
         resolve('------ done -----')
         totalSuccesses++;
       })
@@ -62,14 +61,14 @@ function requestPromise(ipAndPort) {
 
 var minPassed = 0;
 
-function clock() {
-  setTimeout(function() {
-    console.log("-------------- " + (++minPassed) + " minutes ---------------");
-    console.log("Total successes: " + totalSuccesses);
-    clock();
-  }, 60000)
-}
-clock();
+// function clock() {
+//   setTimeout(function() {
+//     console.log("-------------- " + (++minPassed) + " minutes ---------------");
+//     console.log("Total successes: " + totalSuccesses);
+//     clock();
+//   }, 60000)
+// }
+// clock();
 
 process.on('uncaughtException', function(err) {
   runPlayer();
