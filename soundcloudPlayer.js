286004856888064934135d629640a2cc5e1cf3e8
Christian Ayscue <coayscue@gmail.com>
2016-07-15 14:27:26 -0400

    saves

--- soundcloudPlayer.js
+++ soundcloudPlayer.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var http = require('http');
 var Promise = require('promise');
-var trackID = 272922789;
+var trackID = 243711468;
 var totalSuccesses = 0;
-var accessTokenArray = ['ff023a620394846dc2abb3cf6b48e4ea', '134e3bd683d8b4772be6b8c29074b029', '8c0fa73ad5c07f83cb21b257b958daad', '38faf7d0e959c949499860f6443afa66', '281e06c3436513c40ff4bc57b57c3d61', '2a09e9a6824d1e3d2a47a621d506228c', '35b9e9a39309f0826891e014e2bbd1eb', 'dfea0737275304bc5cfa2afa261e5314', '73f248979d28333f1c35b88ba66eefd5', '5844ba4405f47cd6f612ca61cbe0af61', 'b7603ae525e2ed951bd061ad7d7e975b', '4563b0f375b4725196ac6c445e72356f', '3f0e61f44d80a2b81f47ffccebdbdd51', '9a8436077c1191a933534b45b8c99081', '4966765694b42b2269d05c21ada46805', '4103215ce8f4abb5c8f7e76f49801853'];
+var accessTokenArray = ['4720e79b53fc66efaa3818b0992a0284', '5845a670b7680de326470fda240fe602', '373477850828b80b8cae244a4cfe66f5', 'f30dd919b0b02bdfe995966e3e880abb', '5f6af41fad4c66ef0cd83bcb720473cf', '2dd50196cf59c3cca27fc85cb323a37d', '55300fa281c8b2d69f1f710310ca46ff', '1a24eda90ae0d977dae1b7c2b7ccde0d', 'e937b794a67c616bd59eed1a570b44c0', '4a348c975fff6e636f9a861f93eb60d7'];
+//['ff023a620394846dc2abb3cf6b48e4ea', '134e3bd683d8b4772be6b8c29074b029', '8c0fa73ad5c07f83cb21b257b958daad', '38faf7d0e959c949499860f6443afa66', '281e06c3436513c40ff4bc57b57c3d61', '2a09e9a6824d1e3d2a47a621d506228c', '35b9e9a39309f0826891e014e2bbd1eb', 'dfea0737275304bc5cfa2afa261e5314', '73f248979d28333f1c35b88ba66eefd5', '5844ba4405f47cd6f612ca61cbe0af61', 'b7603ae525e2ed951bd061ad7d7e975b', '4563b0f375b4725196ac6c445e72356f', '3f0e61f44d80a2b81f47ffccebdbdd51', '9a8436077c1191a933534b45b8c99081', '4966765694b42b2269d05c21ada46805', '4103215ce8f4abb5c8f7e76f49801853'];
 var ipArray = fs.readFileSync('./proxyList.txt').toString().replace(/(\r\n|\n|\r)/g, '\n').split('\n');
 var ipIndex = Math.floor(Math.random() * ipArray.length);
 for (var i = 0; i < 100; i++) {
@@ -16,10 +17,10 @@ function runPlayer() {
     var ipAndPort = ipArray[ipIndex].split(':');
     requestPromise(ipAndPort)
       .then(function(res) {
-        // console.log(res);
+        console.log(res);
         runPlayer();
       }, function(err) {
-        // console.log(err);
+        console.log(err);
         runPlayer();
       })
   } else {
@@ -50,7 +51,7 @@ function requestPromise(ipAndPort) {
             totalSuccesses++;
           }
         } catch (e) {}
-        resolve('done');
+        resolve(body);
       })
     })
     req.setTimeout(20000, function() {
