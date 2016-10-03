11dac463eb17677b8c48fee3945979928ef8e8cd
root <root@ip-172-31-21-182.us-west-2.compute.internal>
2016-10-03 02:45:06 +0000

    save

--- server/app/notificationCenter/notificationCenter.js
+++ server/app/notificationCenter/notificationCenter.js
@@ -5,19 +5,25 @@ var sendEmail = require('../mandrill/sendEmail.js')
 
 module.exports = {
   sendNotifications: function(userID, type, heading, message, link) {
-    //if user belongs to an admin, use the admins settings
-    User.findById(userID).exec()
+    console.log(userID)
+    console.log(type)
+    console.log(heading)
+    console.log(message)
+    console.log(link)
+    User.findById(userID)
       .then(function(user) {
+        console.log(user);
         if (user.notificationSettings.facebookMessenger[type]) {
-          messengerAPI.buttons(user.notificationSettings.messengerID, heading + ':\n' + message, [{
-            type: "web_url",
-            url: link,
-            title: "View"
-          }]);
+          messengerAPI.buttons(user.notificationSettings.facebookMessenger.messengerID, heading + ':\n' + message, [{
+              type: "web_url",
+              url: link,
+              title: "View"
+            }])
+            .then(console.log, console.log);
         }
         if (user.notificationSettings.email[type]) {
-          sendEmail(user.soundcloud.username, user.notificationSettings.email.email, 'Artists Unlimited Server', 'coayscue@artistsunlimited.co', subject, message + '<br><br><h3><a href=' + link + '>View on artistsunlimited.com</a></h3>');
+          sendEmail(user.soundcloud.username, user.email, 'Artists Unlimited Server', 'coayscue@artistsunlimited.co', heading, message + '<br><br><h3><a href=' + link + '>View on artistsunlimited.com</a></h3>')
         }
-      })
+      }).then(null, console.log);
   }
 }
