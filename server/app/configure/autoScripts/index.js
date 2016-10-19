c898af15ccf0150dc868e5e729c7d92753da304a
root <root@ip-172-31-21-182.us-west-2.compute.internal>
2016-10-19 05:52:54 +0000

    messenger all hooked up

--- server/app/configure/autoScripts/index.js
+++ server/app/configure/autoScripts/index.js
@@ -2,6 +2,5 @@ require('./autoAccessTokenChecker.js')();
 require('./autoReposter.js')();
 require('./autoUnreposter.js')();
 require('./autoPoolSender.js')();
-setTimeout(function() {
-  require('./refundSender.js')();
-}, 2000);
+require('./refundSender.js')();
+require('./reminderMessage.js')();
--- server/app/configure/autoScripts/reminderMessage.js
+++ server/app/configure/autoScripts/reminderMessage.js
@@ -11,28 +11,22 @@ function sendReminder() {
   }, 3600000);
   User.find({
     'notificationSettings.facebookMessenger.lastMessageDate': {
-      $or: [{
-        $lt: (new Date()).getTime() - 21 * 3600000,
-        $gt: (new Date()).getTime() - 22 * 3600000
-      }, {
-        $lt: (new Date()).getTime() - 24 * 3600000,
-        $gt: (new Date()).getTime() - 25 * 3600000
-      }]
+      $lt: (new Date()).getTime() - 24 * 3600000,
+      $gt: (new Date()).getTime() - 25 * 3600000
     }
   }).then(function(users) {
+    var messagedUsers = [];
     users.forEach(function(user) {
-      User.findOne({
-          'paidRepost.userID': user._id
-        })
-        .then(function(adminUser) {
-          if (!adminUser) {
-            messengerAPI.quickReplies(user.messengerID, 'Hey, would you like to receive notifications for the next 24 hours?', [{
-              content_type: "text",
-              title: "Yes",
-              payload: "OPTIN YES"
-            }]);
-          }
-        });
+      if (!messagedUsers.includes(user.notificationSettings.facebookMessenger.messengerID)) {
+        setTimeout(function() {
+          messengerAPI.quickReplies(user.notificationSettings.facebookMessenger.messengerID, 'Hey, would you like to receive notifications for the next 24 hours?', [{
+            content_type: "text",
+            title: "Yes",
+            payload: "OPTIN YES"
+          }]);
+        }, 4000)
+        messagedUsers.push(user.notificationSettings.facebookMessenger.messengerID);
+      }
     });
-  })
+  }).then(null, console.log);
 }
