7454516e9f72f0963d8b33118014a737081dd2ec
Christian Ayscue <coayscue@gmail.com>
2016-12-27 13:23:51 -0500

    save

--- browser/js/common/directives/settings/channelSettings.js
+++ browser/js/common/directives/settings/channelSettings.js
@@ -205,201 +205,201 @@ app.directive('channelsettings', function($http) {
       // $scope.autoFillTracks = [];
 
       // $scope.removeQueueSong = function(song) {
-      // 	var index = $scope.AccountsStepData.queue.indexOf(song.id);
-      // 	$scope.AccountsStepData.queue.splice(index, 1);
-      // 	$scope.saveUser()
-      // 	$scope.loadQueueSongs();
+      //  var index = $scope.AccountsStepData.queue.indexOf(song.id);
+      //  $scope.AccountsStepData.queue.splice(index, 1);
+      //  $scope.saveUser()
+      //  $scope.loadQueueSongs();
       // }
 
       // $scope.addSong = function() {
-      // 	if ($scope.AccountsStepData.queue.indexOf($scope.newQueueID) != -1) return;
-      // 	$scope.AccountsStepData.queue.push($scope.newQueueID);
-      // 	$scope.saveUser();
-      // 	$scope.loadQueueSongs();
+      //  if ($scope.AccountsStepData.queue.indexOf($scope.newQueueID) != -1) return;
+      //  $scope.AccountsStepData.queue.push($scope.newQueueID);
+      //  $scope.saveUser();
+      //  $scope.loadQueueSongs();
       // }
 
       // /*sort start*/
       // $scope.sortableOptions = {
-      // 	stop: function(e, ui) {
-      // 		var logEntry = $scope.autoFillTracks.map(function(i) {
-      // 			return i.id;
-      // 		});
-      // 		$scope.AccountsStepData.queue = logEntry;
-      // 		$scope.saveUser();
-      // 	}
+      //  stop: function(e, ui) {
+      //    var logEntry = $scope.autoFillTracks.map(function(i) {
+      //      return i.id;
+      //    });
+      //    $scope.AccountsStepData.queue = logEntry;
+      //    $scope.saveUser();
+      //  }
       // };
       // /*sort end*/
       // $scope.loadQueueSongs = function(queue) {
-      // 	if ($scope.disallowQueueLoad) return;
-      // 	$scope.disallowQueueLoad = true;
-      // 	setTimeout(function() {
-      // 		$scope.disallowQueueLoad = false;
-      // 	}, 1000);
-      // 	var autofillWidget = SC.Widget('autofillPlayer');
-      // 	$scope.autoFillTracks = [];
-      // 	$scope.AccountsStepData.queue.forEach(function(trackID, index) {
-      // 		SC.get('/tracks/' + trackID)
-      // 			.then(function(data) {
-      // 				$scope.autoFillTracks[index] = data;
-      // 				if (!$scope.$$phase) $scope.$apply();
-      // 			}).then(null, function(err) {
-      // 				if (err.status == 403) {
-      // 					function loadTrack(id, ind) {
-      // 						if (!$scope.loadingAFWidget) {
-      // 							$scope.loadingAFWidget = true;
-      // 							$scope.showAutofillPlayer = true;
-      // 							autofillWidget.load("http://api.soundcloud.com/tracks/" + id, {
-      // 								auto_play: false,
-      // 								show_artwork: false,
-      // 								callback: function() {
-      // 									autofillWidget.getCurrentSound(function(track) {
-      // 										$scope.loadingAFWidget = false;
-      // 										$scope.showAutofillPlayer = false;
-      // 										$scope.autoFillTracks[ind] = track;
-      // 										if (!$scope.$$phase) $scope.$apply();
-      // 									});
-      // 								}
-      // 							});
-      // 						} else {
-      // 							setTimeout(function() {
-      // 								loadTrack(id, ind);
-      // 							}, 300)
-      // 						}
-      // 					}
-      // 					loadTrack(trackID, index);
-      // 				} else if (err.status == 404) {
-      // 					$scope.AccountsStepData.queue.splice(index, 1);
-      // 					$scope.saveUser()
-      // 					$scope.loadQueueSongs();
-      // 				}
-      // 			}).then(null, console.log);
-      // 	});
+      //  if ($scope.disallowQueueLoad) return;
+      //  $scope.disallowQueueLoad = true;
+      //  setTimeout(function() {
+      //    $scope.disallowQueueLoad = false;
+      //  }, 1000);
+      //  var autofillWidget = SC.Widget('autofillPlayer');
+      //  $scope.autoFillTracks = [];
+      //  $scope.AccountsStepData.queue.forEach(function(trackID, index) {
+      //    SC.get('/tracks/' + trackID)
+      //      .then(function(data) {
+      //        $scope.autoFillTracks[index] = data;
+      //        if (!$scope.$$phase) $scope.$apply();
+      //      }).then(null, function(err) {
+      //        if (err.status == 403) {
+      //          function loadTrack(id, ind) {
+      //            if (!$scope.loadingAFWidget) {
+      //              $scope.loadingAFWidget = true;
+      //              $scope.showAutofillPlayer = true;
+      //              autofillWidget.load("http://api.soundcloud.com/tracks/" + id, {
+      //                auto_play: false,
+      //                show_artwork: false,
+      //                callback: function() {
+      //                  autofillWidget.getCurrentSound(function(track) {
+      //                    $scope.loadingAFWidget = false;
+      //                    $scope.showAutofillPlayer = false;
+      //                    $scope.autoFillTracks[ind] = track;
+      //                    if (!$scope.$$phase) $scope.$apply();
+      //                  });
+      //                }
+      //              });
+      //            } else {
+      //              setTimeout(function() {
+      //                loadTrack(id, ind);
+      //              }, 300)
+      //            }
+      //          }
+      //          loadTrack(trackID, index);
+      //        } else if (err.status == 404) {
+      //          $scope.AccountsStepData.queue.splice(index, 1);
+      //          $scope.saveUser()
+      //          $scope.loadQueueSongs();
+      //        }
+      //      }).then(null, console.log);
+      //  });
       // }
       // if ($scope.AccountsStepData && $scope.AccountsStepData.queue && !$scope.alreadyLoaded) {
-      // 	$scope.loadQueueSongs();
-      // 	$scope.alreadyLoaded == true;
+      //  $scope.loadQueueSongs();
+      //  $scope.alreadyLoaded == true;
       // }
       // $scope.choseAutoFillTrack = function(track) {
-      // 	$scope.newQueueID = track.id;
-      // 	$scope.addSong();
+      //  $scope.newQueueID = track.id;
+      //  $scope.addSong();
       // }
 
       // $scope.choseAutoFillPlaylist = function(playlist) {
-      // 	playlist.tracks.forEach(function(track) {
-      // 		if ($scope.AccountsStepData.queue.indexOf(track.id) == -1) $scope.AccountsStepData.queue.push(track.id);
-      // 	})
-      // 	$scope.saveUser();
-      // 	$scope.loadQueueSongs();
+      //  playlist.tracks.forEach(function(track) {
+      //    if ($scope.AccountsStepData.queue.indexOf(track.id) == -1) $scope.AccountsStepData.queue.push(track.id);
+      //  })
+      //  $scope.saveUser();
+      //  $scope.loadQueueSongs();
       // }
 
       // $scope.afcount = 0;
       // $scope.getAutoFillTracks = function() {
-      // 	function waitForAutofill() {
-      // 		$scope.processing = true;
-      // 		setTimeout(function() {
-      // 			if (!$scope.showAutofillPlayer) {
-      // 				$scope.processing = false;
-      // 				if (!$scope.$$phase) $scope.$apply();
-      // 				$scope.getAutoFillTracks();
-      // 			} else {
-      // 				waitForAutofill();
-      // 			}
-      // 		}, 500);
-      // 	}
-      // 	if ($scope.AccountsStepData.queue.length > 0) {
-      // 		if ($scope.autoFillTracks.includes(undefined) || $scope.autoFillTracks.length < $scope.user.AccountsStepData.length) {
-      // 			waitForAutofill();
-      // 			return;
-      // 		}
-      // 		var track = JSON.parse(JSON.stringify($scope.autoFillTracks[$scope.afcount]));
-      // 		$scope.afcount = ($scope.afcount + 1) % $scope.autoFillTracks.length;
-      // 		$scope.makeEvent.trackID = track.id;
-      // 		if (window.location.href.includes('scheduler') && $scope.findUnrepostOverlap() && track.user.id != $scope.user.id) {
-      // 			if ($scope.afcount == 0) {
-      // 				$scope.showPlayer = false;
-      // 				$scope.makeEvent.trackID = undefined;
-      // 				$.Zebra_Dialog("No more autofill songs can be scheduled here. You are not allowed to repost a track within 24 hours of an unrepost of that track or within 48 hours of a repost of the same track.");
-      // 			} else {
-      // 				$scope.makeEvent.trackID = undefined;
-      // 				$scope.getAutoFillTracks();
-      // 			}
-      // 			return;
-      // 		} else {
-      // 			if ($scope.showOverlay) $scope.choseTrack1(track);
-      // 			else $scope.choseTrack(track);
-      // 		}
-      // 	} else {
-      // 		$scope.showOverlay = false;
-      // 		$.Zebra_Dialog('You do not have any tracks by other artists in your autofill list.', {
-      // 			'type': 'question',
-      // 			'buttons': [{
-      // 				caption: 'Cancel',
-      // 				callback: function() {}
-      // 			}, {
-      // 				caption: 'Autofill',
-      // 				callback: function() {
-      // 					$scope.tabSelected = true;
-      // 					$('.nav-tabs a[href="#managereposts"]').tab('show');
-      // 				}
-      // 			}]
-      // 		});
-      // 	}
+      //  function waitForAutofill() {
+      //    $scope.processing = true;
+      //    setTimeout(function() {
+      //      if (!$scope.showAutofillPlayer) {
+      //        $scope.processing = false;
+      //        if (!$scope.$$phase) $scope.$apply();
+      //        $scope.getAutoFillTracks();
+      //      } else {
+      //        waitForAutofill();
+      //      }
+      //    }, 500);
+      //  }
+      //  if ($scope.AccountsStepData.queue.length > 0) {
+      //    if ($scope.autoFillTracks.includes(undefined) || $scope.autoFillTracks.length < $scope.user.AccountsStepData.length) {
+      //      waitForAutofill();
+      //      return;
+      //    }
+      //    var track = JSON.parse(JSON.stringify($scope.autoFillTracks[$scope.afcount]));
+      //    $scope.afcount = ($scope.afcount + 1) % $scope.autoFillTracks.length;
+      //    $scope.makeEvent.trackID = track.id;
+      //    if (window.location.href.includes('scheduler') && $scope.findUnrepostOverlap() && track.user.id != $scope.user.id) {
+      //      if ($scope.afcount == 0) {
+      //        $scope.showPlayer = false;
+      //        $scope.makeEvent.trackID = undefined;
+      //        $.Zebra_Dialog("No more autofill songs can be scheduled here. You are not allowed to repost a track within 24 hours of an unrepost of that track or within 48 hours of a repost of the same track.");
+      //      } else {
+      //        $scope.makeEvent.trackID = undefined;
+      //        $scope.getAutoFillTracks();
+      //      }
+      //      return;
+      //    } else {
+      //      if ($scope.showOverlay) $scope.choseTrack1(track);
+      //      else $scope.choseTrack(track);
+      //    }
+      //  } else {
+      //    $scope.showOverlay = false;
+      //    $.Zebra_Dialog('You do not have any tracks by other artists in your autofill list.', {
+      //      'type': 'question',
+      //      'buttons': [{
+      //        caption: 'Cancel',
+      //        callback: function() {}
+      //      }, {
+      //        caption: 'Autofill',
+      //        callback: function() {
+      //          $scope.tabSelected = true;
+      //          $('.nav-tabs a[href="#managereposts"]').tab('show');
+      //        }
+      //      }]
+      //    });
+      //  }
       // }
 
       // $scope.choseTrack = function(track) {
-      // 	$scope.newQueue = track;
-      // 	$scope.newQueueID = track.id;
+      //  $scope.newQueue = track;
+      //  $scope.newQueueID = track.id;
       // }
       // $scope.trackListChange = function(item) {
-      // 	$scope.newQueueSong = $scope.trackListObj.permalink_url;
-      // 	$scope.choseTrack(item)
+      //  $scope.newQueueSong = $scope.trackListObj.permalink_url;
+      //  $scope.choseTrack(item)
       // };
       // $scope.getTrackListFromSoundcloud = function() {
-      // 	if ($scope.AccountsStepData.submissionData) {
-      // 		var id = $scope.AccountsStepData.submissionData.id ? $scope.AccountsStepData.submissionData.id : $scope.AccountsStepData.submissionData.user.id;
-      // 		$scope.processing = true;
-      // 		SC.get('/users/' + id + '/tracks', {
-      // 				filter: 'public'
-      // 			})
-      // 			.then(function(tracks) {
-      // 				$scope.trackList = tracks;
-      // 				$scope.processing = false;
-      // 				$scope.$apply();
-      // 			})
-      // 			.catch(function(response) {
-      // 				$scope.processing = false;
-      // 				$scope.$apply();
-      // 			});
-      // 	}
+      //  if ($scope.AccountsStepData.submissionData) {
+      //    var id = $scope.AccountsStepData.submissionData.id ? $scope.AccountsStepData.submissionData.id : $scope.AccountsStepData.submissionData.user.id;
+      //    $scope.processing = true;
+      //    SC.get('/users/' + id + '/tracks', {
+      //        filter: 'public'
+      //      })
+      //      .then(function(tracks) {
+      //        $scope.trackList = tracks;
+      //        $scope.processing = false;
+      //        $scope.$apply();
+      //      })
+      //      .catch(function(response) {
+      //        $scope.processing = false;
+      //        $scope.$apply();
+      //      });
+      //  }
       // }
       // $scope.removeQueueSong = function(index) {
-      // 	$scope.AccountsStepData.queue.splice(index, 1);
-      // 	$http.put('/api/database/updateRepostSettings', {
-      // 		queue: $scope.AccountsStepData.queue,
-      // 		id: $scope.AccountsStepData.submissionData.userID
-      // 	}).then(function(res) {
-      // 		SessionService.createAdminUser($scope.AccountsStepData);
-      // 	});
-      // 	$scope.loadQueueSongs();
+      //  $scope.AccountsStepData.queue.splice(index, 1);
+      //  $http.put('/api/database/updateRepostSettings', {
+      //    queue: $scope.AccountsStepData.queue,
+      //    id: $scope.AccountsStepData.submissionData.userID
+      //  }).then(function(res) {
+      //    SessionService.createAdminUser($scope.AccountsStepData);
+      //  });
+      //  $scope.loadQueueSongs();
       // }
 
       // $scope.addSong = function() {
-      // 	if ($scope.AccountsStepData.queue == undefined) {
-      // 		$scope.AccountsStepData.queue = []
-      // 	}
+      //  if ($scope.AccountsStepData.queue == undefined) {
+      //    $scope.AccountsStepData.queue = []
+      //  }
 
-      // 	if ($scope.AccountsStepData.queue.indexOf($scope.newQueueID) != -1) return;
-      // 	$scope.AccountsStepData.queue.push($scope.newQueueID);
-      // 	$http.put('/api/database/profile', {
-      // 		queue: $scope.AccountsStepData.queue,
-      // 		_id: $scope.AccountsStepData.submissionData.userID
-      // 	}).then(function(res) {
-      // 		SessionService.createAdminUser($scope.AccountsStepData);
-      // 	});
-      // 	$scope.newQueueSong = undefined;
-      // 	$scope.trackListObj = "";
-      // 	$scope.newQueue = undefined;
-      // 	$scope.loadQueueSongs();
+      //  if ($scope.AccountsStepData.queue.indexOf($scope.newQueueID) != -1) return;
+      //  $scope.AccountsStepData.queue.push($scope.newQueueID);
+      //  $http.put('/api/database/profile', {
+      //    queue: $scope.AccountsStepData.queue,
+      //    _id: $scope.AccountsStepData.submissionData.userID
+      //  }).then(function(res) {
+      //    SessionService.createAdminUser($scope.AccountsStepData);
+      //  });
+      //  $scope.newQueueSong = undefined;
+      //  $scope.trackListObj = "";
+      //  $scope.newQueue = undefined;
+      //  $scope.loadQueueSongs();
       // }
 
       $scope.saveUser = function() {
@@ -419,53 +419,53 @@ app.directive('channelsettings', function($http) {
       // var tmpList = [];
       // $scope.sortingLog = [];
       // $scope.sortableOptions = {
-      // 	update: function(e, ui) {
-      // 		var logEntry = tmpList.map(function(i) {
-      // 			return i.id;
-      // 		});
-      // 		$scope.AccountsStepData.queue = [];
-      // 		$scope.sortingLog.push('Update: ' + logEntry);
-      // 		$scope.AccountsStepData.queue = logEntry;
-      // 		//$scope.saveUser();
-      // 	},
-      // 	stop: function(e, ui) {
-      // 		// this callback has the changed model
-      // 		var logEntry = tmpList.map(function(i) {
-      // 			return i.id;
-      // 		});
-      // 		$scope.AccountsStepData.queue = [];
-      // 		$scope.sortingLog.push('Stop: ' + logEntry);
-      // 		$scope.AccountsStepData.queue = logEntry;
-      // 		$scope.saveUser();
-      // 	}
+      //  update: function(e, ui) {
+      //    var logEntry = tmpList.map(function(i) {
+      //      return i.id;
+      //    });
+      //    $scope.AccountsStepData.queue = [];
+      //    $scope.sortingLog.push('Update: ' + logEntry);
+      //    $scope.AccountsStepData.queue = logEntry;
+      //    //$scope.saveUser();
+      //  },
+      //  stop: function(e, ui) {
+      //    // this callback has the changed model
+      //    var logEntry = tmpList.map(function(i) {
+      //      return i.id;
+      //    });
+      //    $scope.AccountsStepData.queue = [];
+      //    $scope.sortingLog.push('Stop: ' + logEntry);
+      //    $scope.AccountsStepData.queue = logEntry;
+      //    $scope.saveUser();
+      //  }
       // };
       // /*sort end*/
       // $scope.loadQueueSongs = function(queue) {
-      // 	setTimeout(function() {
-      // 		if ($scope.AccountsStepData.queue == undefined) {
-      // 			$scope.AccountsStepData.queue = [];
-      // 		}
-      // 		var i = 0;
-      // 		$scope.autoFillTracks = [];
-      // 		$scope.AccountsStepData.queue.forEach(function(songID) {
-      // 			SC.get('/tracks/' + songID)
-      // 				.then(function(track) {
-      // 					$scope.autoFillTracks.push(track);
-      // 					i++;
-      // 					tmpList = $scope.autoFillTracks;
-      // 					$scope.list = tmpList;
-      // 					$scope.$digest();
-      // 				}, console.log);
-      // 		})
-      // 	}, 3000);
+      //  setTimeout(function() {
+      //    if ($scope.AccountsStepData.queue == undefined) {
+      //      $scope.AccountsStepData.queue = [];
+      //    }
+      //    var i = 0;
+      //    $scope.autoFillTracks = [];
+      //    $scope.AccountsStepData.queue.forEach(function(songID) {
+      //      SC.get('/tracks/' + songID)
+      //        .then(function(track) {
+      //          $scope.autoFillTracks.push(track);
+      //          i++;
+      //          tmpList = $scope.autoFillTracks;
+      //          $scope.list = tmpList;
+      //          $scope.$digest();
+      //        }, console.log);
+      //    })
+      //  }, 3000);
 
       // }
       // $scope.loadQueueSongs();
       // $scope.choseAutoFillTrack = function(track) {
-      // 		$scope.searchString = track.title;
-      // 		$scope.newQueueID = track.id;
-      // 		$scope.addSong();
-      // 	}
+      //    $scope.searchString = track.title;
+      //    $scope.newQueueID = track.id;
+      //    $scope.addSong();
+      //  }
       /*Repost settings end*/
       $scope.finishAdmin = function() {
         $state.go("accounts");
